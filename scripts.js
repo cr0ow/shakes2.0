4860bcdd3943f2daa30d4b5d79801bca302fdf93
cr0ow <70718059+cr0ow@users.noreply.github.com>
2021-12-28 17:27:20 +0100

    przycisk powrotu do miasta
    Dodany przycisk powrotu do miasta - ekranu startowego.
    Zmienione tło poza głównym contentem.

--- scripts.js
+++ scripts.js
@@ -1,38 +1,51 @@
 function switchView(name) {
     const content = document.getElementById('content')
     content.innerHTML = null
+
+    const back = document.createElement('button')
+    back.classList = 'exitButton'
+    back.setAttribute('onclick','back()')
+
+
     switch(name) {
         case 'karczma':
-            content.setAttribute('style','background-image:url(img/tavern.png)')
+            content.style.backgroundImage = 'url(img/tavern.png)'
             break
         case 'arena':
-            content.setAttribute('style','background-image:url(img/arena.png)')
+            content.style.backgroundImage = 'url(img/arena.png)'
             break
         case 'zbrojownia':
-            content.setAttribute('style','background-image:url(img/weapon_shop.png)')
+            content.style.backgroundImage = 'url(img/weapon_shop.png)'
             break
         case 'gabinet':
-            content.setAttribute('style','background-image:url(img/magic_shop.png)')
+            content.style.backgroundImage = 'url(img/magic_shop.png)'
             break
         case 'stajnia':
-            content.setAttribute('style','background-image:url(img/stable.png)')
+            content.style.backgroundImage = 'url(img/stable.png)'
             break
         case 'lochy':
-            content.setAttribute('style','background-image:url(img/dungeon.png)')
+            content.style.backgroundImage = 'url(img/dungeon.png)'
             break
         case 'twierdza':
-            content.setAttribute('style','background-image:url(img/fortress.png)')
+            content.style.backgroundImage = 'url(img/fortress.png)'
             break
         case 'gildia':
-            content.setAttribute('style','background-image:url(img/guild.png)')
+            content.style.backgroundImage = 'url(img/guild.png)'
             break
         case 'poczta':
-            content.setAttribute('style','background-image:url(img/post.png)')
+            content.style.backgroundImage = 'url(img/post.png)'
             break
         case 'sala chwały':
-            content.setAttribute('style','background-image:url(img/fame.png)')
+            content.style.backgroundImage = 'url(img/fame.png)'
             break
     }
+    content.append(back);
+}
+
+function back() {
+    const content = document.getElementById('content')
+    content.innerHTML = null
+    content.style.backgroundImage = 'url(img/town.png)'
 }
 
 function options() {
